82ebbafef6aab3c1556a8a89070d91d14f09eb13
B.Bob <okakax@gmail.com>
2024-01-20 20:20:34 +0900

    tsc通過

--- suburi/src/AppLayout.tsx
+++ suburi/src/AppLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import "./AppLayout.css";
 
 import Header from "./components/Header";
 
-export default function AppLayout({children}) {
+export default function AppLayout({children}: {children?: React.JSX.Element}) {
   return (
     <Grid container direction="column">
         <Grid>
